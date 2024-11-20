47b3d039eb5a069de20b4c3ea687a9c999b0181b
duuun <1278449599@qq.com>
2024-11-20 17:00:07 +0800

    feat: 代码风格及规范配置

--- eslint.config.js
+++ eslint.config.js
@@ -6,14 +6,38 @@ export default [
   {
     name: 'app/files-to-lint',
     files: ['**/*.{js,mjs,jsx,vue}'],
-  },
+    rules: {
+      // 配置 Prettier 规则
+      'prettier/prettier': [
+        'warn',
+        {
+          singleQuote: true, // 单引号
+          semi: false, // 无分号
+          printWidth: 80, // 每行宽度至多80字符
+          trailingComma: 'none', // 不加对象|数组最后逗号
+          endOfLine: 'auto' // 换行符号不限制（win mac 不一致）
+        }
+      ],
+      // Vue 相关规则
+      'vue/multi-word-component-names': [
+        'warn',
+        {
+          ignores: ['index'] // vue组件名称多单词组成（忽略index.vue）
+        }
+      ],
+      'vue/no-setup-props-destructure': 'off', // 关闭 props 解构的校验
 
+      // 通用规则
+      'no-undef': 'error' // 禁止未定义的变量
+    }
+  },
   {
     name: 'app/files-to-ignore',
-    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**']
   },
 
+  // 引入推荐配置
   js.configs.recommended,
   ...pluginVue.configs['flat/essential'],
-  skipFormatting,
+  skipFormatting
 ]
